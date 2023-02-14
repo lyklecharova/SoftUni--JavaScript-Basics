4b2a8395bc37fc5993c12c2def690d10293f3ac1
Lilyana Klecharova <lklecharova9@gmail.com>
2023-02-14 15:30:39 +0200

    Delete Regular Expressions - Lab directory

--- Regular Expressions - Lab/matchFullName.js
+++
@@ -1,8 +0,0 @@
-function matchFullName(input) {
-    let text = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/mg
-    let name = input.match(text);
-    console.log(name.join(' '));
-}
-matchFullName("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"
-)
-//Ivan Ivanov Test Testov
